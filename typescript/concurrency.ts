925c8a3b796a997317462c41703ce8b44b1d291d
Oleksii Naboichenko <oleksii.naboichenko@droneup.com>
2025-03-01 15:06:20 -0500

    Fixed TS warnings

--- typescript/concurrency.ts
+++ typescript/concurrency.ts
@@ -1,18 +1,29 @@
-const { Worker } = require('worker_threads');
+import { Worker } from 'worker_threads';
 
 const start = Date.now();
 let completed = 0;
 
+const workerScript = `
+    const { parentPort } = require('worker_threads');
+    let sum = 0;
+    for (let i = 0; i < 1000000; i++) sum += i;
+    parentPort.postMessage(sum);
+`;
+
 for (let i = 0; i < 100; i++) {
-    new Worker(`
-        const { parentPort } = require('worker_threads');
-        let sum = 0;
-        for (let i = 0; i < 1000000; i++) sum += i;
-        parentPort.postMessage(sum);
-    `, { eval: true }).on('message', () => {
+    const worker = new Worker(workerScript, { eval: true });
+    worker.on('message', () => {
         completed++;
         if (completed === 100) {
             console.log(`Time: ${Date.now() - start} ms`);
+        }
+    });
+    worker.on('error', (err) => {
+        console.error(`Worker error: ${err}`);
+    });
+    worker.on('exit', (code) => {
+        if (code !== 0) {
+            console.error(`Worker stopped with exit code ${code}`);
         }
     });
 }
